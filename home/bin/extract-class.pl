b75f5d3ce91a24a3b621f01e71d085d0839e64df
Josh Kelley <joshkel@gmail.com>
2018-03-16 15:20:48 -0400

    Improve extract-function; add extract-class
    Allow leading whitespace to handle languages like C# and Java (and
    inline functions in C++).
    
    Strip carriage returns, since these were causing some `^M` to show up
    when run from git-bash.
    
    extract-class is just a copy of extract-function.  Ugly, but easy.

--- home/bin/extract-class.pl
+++ home/bin/extract-class.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/perl
-# Simple script to extract a function from a curly-brace-language source file
+# Simple script to extract a class from a curly-brace-language source file
 
 use strict;
 use re 'eval';
 use File::Basename;
 use Text::Balanced qw(extract_bracketed);
 
-my $function = shift(@ARGV);
+my $class = shift(@ARGV);
 
 foreach my $file (@ARGV) {
 	local $/;
 	open SRC, $file or (warn "Unable to open $file" and next);
 	my $src = <SRC>;
 	my($body, $remainder);
-	if ($src =~ /^\s*((?:\w+\s+)+(?:\w+::)?$function\s*\([^)]*\)\s*(?={))/mg and (($body, $remainder) = extract_bracketed($src, '{}'))) {
+if ($src =~ /^\s*((?:\w+\s+)+(?:\w+::)?$class\s*:.*?(?={))/msg and (($body, $remainder) = extract_bracketed($src, '{}'))) {
 		my ($declaration, $comment, $previous);
 		$declaration = $1;
 		$previous = $`;
@@ -24,7 +24,7 @@ foreach my $file (@ARGV) {
         my $output = "$comment$declaration$body";
         $output =~ s/\r//g;
 
-		open OUT, "> $function-" . basename($file);
+		open OUT, "> $class-" . basename($file);
 		print OUT "$output\n";
 		close OUT;
 
