773a6ed4b3d54ecf47232538e465df8dafa36efa
Josh Kelley <josh.kelley@nih.gov>
2019-10-17 18:17:41 -0400

    Automatically autoload Composer dependencies
    https://github.com/bobthecow/psysh/wiki/Configuration#automatically-load-composer-dependencies

--- home/.config/psysh/config.php
+++ home/.config/psysh/config.php
@@ -1,4 +1,10 @@
 <?php
+
+// Automatically autoload Composer dependencies
+if (is_file(getcwd() . '/vendor/autoload.php')) {
+    require_once getcwd() . '/vendor/autoload.php';
+}
+
 return [
     # Workaround for https://github.com/bobthecow/psysh/issues/540
     'usePcntl' => false,
