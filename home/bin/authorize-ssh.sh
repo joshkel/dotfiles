9266d115f2aa301386cb0b6c77ec15c7b206fdf3
Josh Kelley <josh.kelley@nih.gov>
2019-10-28 14:25:12 -0400

    Add comments and restorecon command

--- home/bin/authorize-ssh.sh
+++ home/bin/authorize-ssh.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#
+# Installs an SSH key to a remote system.
+#
+# Your system probably provides an ssh-copy-id command that does more or less
+# the same thing.
 
 set -e
 
-< ~/.ssh/id_rsa.pub ssh -o StrictHostKeyChecking=no "$1" 'umask 077 && mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys'
+< ~/.ssh/id_rsa.pub ssh -o StrictHostKeyChecking=no "$1" 'umask 077 && mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && (if command -v restorecon; restorecon -R ~; fi )'
