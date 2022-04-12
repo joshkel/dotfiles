6c40efa606a98036f900e781642f4d12b9432d20
Josh Kelley <joshkel@gmail.com>
2022-04-12 16:44:59 -0400

    Add git-resolve-yarn; update IPython for new version; add `git co`

--- home/.ipython/profile_default/startup/10-autoload.py
+++ home/.ipython/profile_default/startup/10-autoload.py
@@ -1,3 +1,5 @@
 # https://stackoverflow.com/a/32683001/25507
-get_ipython().magic("%reload_ext autoreload")
-get_ipython().magic("%autoreload 2")
+# get_ipython().magic("%reload_ext autoreload")
+# get_ipython().magic("%autoreload 2")
+get_ipython().run_line_magic("reload_ext", "autoreload")
+get_ipython().run_line_magic("autoreload", "2")
